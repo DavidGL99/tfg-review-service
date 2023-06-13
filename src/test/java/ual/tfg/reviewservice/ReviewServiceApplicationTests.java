a9bc223879f5ba26e6917b15dcafa92d7ea99672
dgomezl <david.parador99@gmail.com>
2023-06-13 09:59:35 +0200

    deploy to aws

---
+++ src/test/java/ual/tfg/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package ual.tfg.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
